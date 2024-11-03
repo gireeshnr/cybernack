d78b4c11374979ec9504edcb438cf89b1b73f0d1
GP <gireesh@mac.home>
2024-11-03 22:07:55 +0100

    Temporary rename for case sensitivity

--- server/src/models/temp.js
+++ server/src/models/temp.js
@@ -1,16 +1,24 @@
 import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
-const SubscriptionSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: String,
-  priceMonthly: { type: Number, required: true },
-  priceYearly: { type: Number, required: true },
-  features: [String],  // Array of feature strings
-  modules: [String],   // Array of module names associated with the subscription
-  isActive: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now },
+const OrganizationSchema = new Schema({
+  name: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  subscription: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Subscription',
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: true, // Default to true for newly created organizations
+  },
 });
 
-// Ensure the model is only compiled once
-const Subscription = mongoose.models.Subscription || mongoose.model('Subscription', SubscriptionSchema);
-export default Subscription;
+// Check if the model already exists to avoid re-compilation
+const Organization = mongoose.models.Organization || mongoose.model('Organization', OrganizationSchema);
+
+export default Organization;
